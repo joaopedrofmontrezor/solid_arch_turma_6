dfc98a18cb1273e1cdb0d2772a7b212b1b6010a8
João Pedro Ferreira Montrezor <jp.montrezor@gmail.com>
2026-04-15 12:42:39 -0300

    alteração UserController

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,58 @@ const User = require('../models/User')
 
 module.exports = class UserController {
     static async register(req, res) {
-        res.json('Olá Get Pet')
+        const { name, email, phone, password, confirmpassword } = req.body
+
+        if (!name) {
+            res.status(422).json({message: 'Nome é obrigatório'})
+            return
+        }
+
+        if (!phone) {
+            res.status(422).json({message: 'Telefone é obrigatório'})
+            return
+        }
+
+        if (!password) {
+            res.status(422).json({message: 'Senhaé obrigatório'})
+            return
+        }
+
+        if (!confirmpassword) {
+            res.status(422).json({message: 'Confirmação de senha é obrigatório'})
+            return
+        }
+
+        if (password !== confirmpassword) {
+            res.status(422).json({message: 'As senhas não coincidem'})
+            return
+        }
+
+        const userExist = await User.findOne({email:email})
+
+        if(userExist) {
+            res.status(422).json({message: 'O usuário já existe em nossos registros.'})
+            return
+        }
+
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        const user = new User({
+            name,
+            email,
+            phone,
+            password : passwordHash,
+        })
+        
+        try{
+            const newUser = await user.save()
+            res.status(201).json({
+                message: 'Usuário criado no Get Pet.',
+                newUser
+            })
+        } catch (error) {
+            res.status(503).json({message: error})
+        }
     }
 }
