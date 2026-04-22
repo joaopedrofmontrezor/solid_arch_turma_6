34ae8633c067ae784926c6ccd7e9e1b7f9d443ae
João Pedro Ferreira Montrezor <jp.montrezor@gmail.com>
2026-04-22 12:50:19 -0300

    Aula de jwt implementação inicial

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const createUserToken = require('../helpers/create-user-token')
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -49,10 +50,7 @@ module.exports = class UserController {
         
         try{
             const newUser = await user.save()
-            res.status(201).json({
-                message: 'Usuário criado no Get Pet.',
-                newUser
-            })
+            await createUserToken(newUser, req, res)
         } catch (error) {
             res.status(503).json({message: error})
         }
