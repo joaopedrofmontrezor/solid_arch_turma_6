37245cff17fc3c54e52298166c347388a86b01aa
João Pedro Ferreira Montrezor <jp.montrezor@gmail.com>
2026-05-06 13:01:24 -0300

    Finalização para Prova 1

--- backend/routers/UserRouters.js
+++ backend/routers/UserRouters.js
@@ -1,14 +1,14 @@
 const router = require('express').Router()
-
+ 
 const UserController = require('../controllers/UserController')
-
+ 
 const verifyToken = require('../helpers/verify-token')
-
+const { imageUpload } = require('../helpers/image-upload')
+ 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
-router.get('/checkuser', UserController.checkUser)
-router.get('/:id', UserController.getUserById)
-router.patch('/edit/:id', verifyToken, UserController.editUser)
-
-
+router.get("/checkUser", UserController.checkUser)
+router.get("/getUser", UserController.getUserById)
+router.patch("/:edit/:id", verifyToken,imageUpload.single('image'), UserController.editUser)
+ 
 module.exports = router
